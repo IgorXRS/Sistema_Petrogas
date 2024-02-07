828cf5e76b953b23dceb7a15ac12ef7a30462b56
Igor Xavier <igorxrds@gmail.com>
2024-02-07 08:16:02 -0300

    Update home.js

--- Modulos/home.js
+++ Modulos/home.js
@@ -1,3 +1,12 @@
+function telaCheia () {
+    let containerHome01 = document.querySelector('.container-home-01');
+    let containerHome02 = document.querySelector('.container-home-02');
+
+    containerHome02.classList.toggle('telaCheia');
+    containerHome01.classList.toggle('Ocultar');
+
+};
+
 document.addEventListener('DOMContentLoaded', function () {
     checkAlert();
     setInterval(checkAlert, 1800000); // Verifica a cada 30 minutos
